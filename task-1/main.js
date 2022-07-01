45c7f808a5be85528a080e49b57dd0a1b26ac339
Vusal <105041599+VusalRzali@users.noreply.github.com>
2022-07-01 12:35:22 +0400

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -1,8 +1,20 @@
-// Use this matrix for solution
 let matrix = [
-    [4, -2, 5],
-    [1, -4, -12],
-    [0, 1, -3]
-]
-
-// Write code here. 
+  [4, -2, 5],
+  [1, -4, -12],
+  [0, 1, -3],
+];
+let n = 3;
+let counter = 0;
+for (let i = 0; i < n; i++) {
+  matrix.push(new Array(n));
+}
+let sum = 0;
+for (let i = 0; i < n; i++) {
+  for (let j = 0; j < n; j++) {
+    if (matrix[i][j] < 0 && matrix[i][j] % 2 == 0) {
+      sum = sum + matrix[i][j];
+      counter++;
+    }
+  }
+}
+console.log("" + counter + " " + sum);
