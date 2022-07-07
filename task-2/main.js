33299aa70acf9437f2a207d8d4c552297a3ac5b7
Vusal <105041599+VusalRzali@users.noreply.github.com>
2022-07-07 15:34:00 +0400

    Update main.js

--- task-2/main.js
+++ task-2/main.js
@@ -4,15 +4,15 @@ let matrix = [
     [9, 1, 2, 3],
     [4, 5, 6, 7]
 ];
-let matrix1 = [];
-let r = 3;
+let r = 3;          
 let c = 2;
-for (let i = 0; i < r; i++) {
-  matrix1.push(new Array(c));
+let str = ''
+
+for(let i = 0; i < r; i++){
+    for(let j = 0;j < c; j++){
+        str += matrix[i][j] + ' ';
+    }
+    str = str.trim()
+    str += '\n';
 }
-for (let i = 0; i < r; i++) {
-  for (let j = 0; j < c; j++) {
-    matrix1[i][j] = matrix[i][j];
-  }
-}
-console.log(matrix1);
+console.log(str);
