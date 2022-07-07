e9efe75910b72d7566024424fe6518328027b7f4
Vusal <105041599+VusalRzali@users.noreply.github.com>
2022-07-07 13:09:42 +0400

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -1,10 +1,26 @@
-// Use this matrix for solution
 
+let x1 = 4;
+let x2 = 5;
 let matrix = [
-    [2, 5, 3, 1, 4],
-    [3, 5, 2, 3, 4],
-    [4, 6, 1, 2, 3],
-    [4, 5, 6, 7, 7]
-]
-
-// Write code here. 
+  [2, 5, 3, 1, 4],
+  [3, 5, 2, 3, 4],
+  [4, 6, 1, 2, 3],
+  [4, 5, 6, 7, 7],
+];
+let min = matrix[0][0];
+for (let i = 0; i < x1; i++) {
+  for (let j = 0; j < x2; j++) {
+    if (matrix[i][j] < min) {
+      min = matrix[i][j];
+    }
+  }
+}
+let ch = "";
+for (let i = 0; i < x1; i++) {
+  for (let j = 0; j < x2; j++) {
+    if (matrix[i][j] == min) {
+      ch = ch + (i + 1) + " ";
+    }
+  }
+}
+console.log(ch);
